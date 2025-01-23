6ed5cd9ec9d60f51c9e2437dcf7d554a86b71865
estheredi0406 <e.sessenou@alustudent.com>
2025-01-23 14:44:16 +0200

    another try

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,6 +4,9 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 --create a new user in  the localhost with all privileges--
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
+--Print a confirmation message--
+SELECT 'hbnb_dev_db' AS database_name;
+
 --grant all privileges to the user in the database--
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
