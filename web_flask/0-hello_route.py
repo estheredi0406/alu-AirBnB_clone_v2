59bf408a6b8efd67c4cff9e15ec91c147582cd3c
estheredi0406 <e.sessenou@alustudent.com>
2025-02-21 22:01:15 +0200

    file made executable

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-        """Returns a string at the root route"""
-            return 'Hello HBNB!'
+    """Returns a string at the root route"""
+    return 'Hello HBNB!'
 
 
-        if __name__ == '__main__':
-                app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
